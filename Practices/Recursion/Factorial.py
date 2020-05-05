b5034c039d0367f725d6e1b907004fcd85e88282
waiyulam <waiyu0616@outlook.com>
2020-05-04 17:55:39 -0700

    update Factorial

--- Practices/Recursion/Factorial.py
+++ Practices/Recursion/Factorial.py
@@ -1,11 +1,14 @@
 
 #!/bin/python
-
+import sys
 def factorial(n):
+    F = 1
     if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
+        return F
+    print("I am calculating the F({})".format(n))
+    F = n * factorial(n-1)
+    print("Done, F({}) is equal to {}".format(n,F))
+    return F
 
 u_input = int(input("Input a number to compute the factorial:  "))
 print(factorial(u_input))
