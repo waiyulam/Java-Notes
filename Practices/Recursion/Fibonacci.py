9d1a4d47f5dd29de3d0062263b1754eef7aacfc8
waiyulam <waiyu0616@outlook.com>
2020-05-05 16:15:54 -0700

    Fibonacci memoization

--- Practices/Recursion/Fibonacci.py
+++ Practices/Recursion/Fibonacci.py
@@ -2,6 +2,9 @@
 import sys
 import time 
 # Albert : iteration solution 
+# Fibonacci_cache = {}
+Fibonacci_cache = {}
+
 def Fib_A(n):
     if n <= 1: 
         return n
@@ -21,10 +24,23 @@ def Fib_P(n):
         return n 
     return Fib_P(n-1) + Fib_P(n-2)
 
+# Pinto with memoization 
+def Fib_M(n):
+    if n<=1:
+        return n 
+    elif n in Fibonacci_cache:
+        return Fibonacci_cache[n]
+    Fibonacci_cache[n] = Fib_M(n-1) + Fib_M(n-2) 
+    return Fibonacci_cache[n] 
 
 if __name__ == "__main__":
     n = int(input("Give me a n : "))
     start = time.time()
-    print("The Fibonacci number {} is {} ".format(n,Fib_A(n)))
+    print("The Fibonacci number {} is {} ".format(n,Fib_P(n)))
     end = time.time()
     print("Total time : {}".format(end-start))
+    #Implement recursive function with memoization 
+    start = time.time()
+    print("The Fibonacci number w/ memoization {} is {} ".format(n,Fib_M(n)))
+    end = time.time()
+    print("Total time w/ memoization : {}".format(end-start))
