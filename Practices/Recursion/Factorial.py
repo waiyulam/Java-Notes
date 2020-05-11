210b3381ffe6c43126754012ca0f7f0a3d29f636
waiyulam <waiyu0616@outlook.com>
2020-05-11 15:53:25 -0700

    add majority element practice

--- Practices/Recursion/Factorial.py
+++ Practices/Recursion/Factorial.py
@@ -5,7 +5,7 @@ def factorial(n):
     F = 1
     if n == 0:
         return F
-    print("I am calculating the F({})".format(n))
+    print("I am calculating the F({}) ... ".format(n))
     F = n * factorial(n-1)
     print("Done, F({}) is equal to {}".format(n,F))
     return F
