2b6f34b59159b2b690e158fe1bbdd2e8ddbe73f6
waiyulam <waiyu0616@outlook.com>
2020-06-25 19:51:50 -0700

    add perfect square

---
+++ Practices/dp/perfect_square.py
@@ -0,0 +1,27 @@
+# Improving coin change bottom up dp algorithm
+# perfect square: https://leetcode.com/problems/perfect-squares/ 
+import sys 
+
+class Solution(object):
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if (n == 1):
+            return 1
+        dp = [sys.maxsize for i in range(0,n+1)]
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2,n+1):
+            p = 1
+            while pow(p,2) <= i:
+                dp[i] = min(dp[i], dp[i-pow(p,2)] + 1)
+                p += 1
+        return dp[n]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    n = 13
+    print("output : {}".format(s.numSquares(n)))
